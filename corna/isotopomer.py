0edb90b4a6582f14ba59205db5d8a77efe43b6ba
Raaisa <raaisa@elucidata.io>
2016-05-12 19:02:28 +0530

    create_fragment and create_isotopomer_label

--- corna/isotopomer.py
+++ corna/isotopomer.py
@@ -1,8 +1,30 @@
 from model import Fragment
+from helpers import label_dict_to_key
+
+def create_fragment(name, formula):
+    frag = Fragment(name, formula)
+    frag_key = name + '_' + formula
+    return {frag_key: frag}
+
+def create_isotopomer_label(frag_dict, label_dict):
+    frag_key, frag = frag_dict.items()[0]
+    frag.check_if_valid_label(label_dict)
+    label_key = label_dict_to_key(label_dict)
+    label = {label_key: label_dict}
+    return {frag_key: label}, frag_dict
 
 def create_isotopomers_from_label(name, formula, label_dict_list):
     frag = Fragment(name, formula)
+    frag_key = name + '_' + formula
+    label_key_list = []
+    label_labdict = {}
     for label_dict in label_dict_list:
         frag.check_if_valid_label(label_dict)
+        label_key = label_dict_to_key(label_dict)
+        label_key_list.append(label_key)
+        label_labdict[label_key] = label_dict
+    return {frag_key : label_key_list}, label_labdict, {frag_key:frag}
 
-    return {frag : label_dict_list}
+# def add_data_isotopomers(isotopomer, label_intensity, label_key_list):
+#     for label in label_key_list:
+
--- tests/test_isotopomer.py
+++ tests/test_isotopomer.py
@@ -1,4 +1,27 @@
 import corna.isotopomer as iso
+from corna.model import Fragment
 
-def test_create_isotopomer_from_label():
-    iso.create_isotopomer_from_label()
+class TestFragmentClass:
+    @classmethod
+    def setup_class(cls):
+        cls.fragment = Fragment('Glucose', 'C6H12O6')
+        cls.frag_key = 'glucose_C6H12O6'
+        cls.label_dict = {'C13':4}
+        cls.label_key = 'C13_4'
+
+    @classmethod
+    def teardown_class(cls):
+        del cls.fragment
+        del cls.frag_key
+
+    def test_create_fragment(self):
+        assert isinstance(iso.create_fragment('glucose', 'C6H12O6')[self.frag_key], Fragment)
+
+    def test_create_isotopomer_label(self):
+        assert iso.create_isotopomer_label({self.frag_key: self.fragment}, self.label_dict)[0][self.frag_key]\
+               == {self.label_key:self.label_dict}
+
+    # def test_create_isotopomer_from_label():
+    #     isotopomer, label_dicts, frag_dict = iso.create_isotopomers_from_label('glucose', 'C6H12O6', [{'C13':2}, {'C13':3}, {'C13':4}])
+    #     frag = 'glucose_C6H12O6'
+    #     assert isotopomer[frag] == ['C13_2', 'C13_3', 'C13_4']
