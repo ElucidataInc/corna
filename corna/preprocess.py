13ef0d419165f8c003d1fd44d6bb9a54e7d3c60f
Raaisa <raaisa@elucidata.io>
2016-05-15 16:31:23 +0530

    bacground noise and subtraction

--- corna/preprocess.py
+++ corna/preprocess.py
@@ -7,3 +7,12 @@ def background_noise(parent_label, na, daughter_atoms, unlabel_intensity):
     else:
         noise = unlabel_intensity*math.pow(na, parent_label)
     return noise
+
+def backround_subtraction(input_intensity, noise):
+    intensity = input_intensity - noise
+    if intensity < 0:
+        return 0
+    else:
+        return intensity
+
+#def background_correction(sample_name, combined_fragment, unlabeled_fragment):
