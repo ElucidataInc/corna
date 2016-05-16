3bdfe7f66f9ef9f6952d16118f5e1aaa20a72348
Sini Nagpal <sini.nagpal@elucidata.io>
2016-05-16 09:56:26 +0530

    preproc tested

--- corna/nacorr.py
+++ corna/nacorr.py
@@ -1,8 +1,8 @@
-import file_parser as fp
 import os
 import sys
 
 import helpers as hl
+import file_parser as fp
 import isotopomer as iso
 import preprocess as preproc
 
@@ -25,7 +25,7 @@ merged_df = fp.maven_merge_dfs(input_data, metadata)
 filter_df = hl.filter_df(merged_df, 'Sample Name', 'sample_1')
 
 # std model maven
-std_model = fp.standard_model(merged_df, parent = 'False')
+std_model = fp.standard_model(merged_df, parent = False)
 
 
 # MultiQuant
@@ -39,7 +39,7 @@ merged_data = fp.mq_merge_dfs(mq_df, mq_metdata)
 merged_data.to_csv(data_dir + '/merged_mq.csv')
 
 # standard model mq
-std_model_mq = fp.standard_model(merged_data, parent = 'true')
+std_model_mq = fp.standard_model(merged_data, parent = True)
 
 # integrating isotopomer and parser (input is standardised model in form of nested dictionaries)
 fragments_dict = {}
@@ -47,8 +47,9 @@ for key, value in std_model_mq.iteritems():
 	 fragments_dict.update(iso.bulk_insert_data_to_fragment(key, value, mass=True, number=False, mode=None))
 
 # preprocessing : correction for background noise
-print preproc.background('A. [13C-glc] G2.5 0min', fragments_dict[('Glutamate 147/41_147.0', 'Glutamate 147/41_41.0')],\
+preprocess_data = preproc.background('A. [13C-glc] G2.5 0min', fragments_dict[('Glutamate 147/41_147.0', 'Glutamate 147/41_41.0')],\
  fragments_dict[('Glutamate 146/41_146.0', 'Glutamate 146/41_41.0')])
+
 
 
 
