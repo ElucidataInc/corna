66760cdb828167d88a60c68017aa758b1e0ed203
Raaisa <raaisa@elucidata.io>
2016-05-17 02:09:48 +0530

    arrane_framents_by_mass

--- corna/algorithms.py
+++ corna/algorithms.py
@@ -1,9 +1,22 @@
-def na_correct_mimosa(frag_m_n, intensity_m_n, intensity_m_1_n, intensity_m_1_n_1, isotope, na):
-    frag_m = frag_m_n[0]
-    frag_n = frag_m_n[1]
-    p = frag_m.get_labeled_elem_num(isotope)
-    d = frag_n.get_labeled_elem_num(isotope)
-    m = frag_m.get_num_labeled_atoms(isotope)
-    n = frag_n.get_num_labeled_atoms(isotope)
+def na_correct_mimosa_algo(parent_frag_m, daughter_frag_n, intensity_m_n, intensity_m_1_n, intensity_m_1_n_1,
+                      isotope, na):
+    p = parent_frag_m.get_number_of_atoms_isotope(isotope)
+    d = daughter_frag_n.get_number_of_atoms_isotope(isotope)
+    m = parent_frag_m.get_num_labeled_atoms_tracer()
+    n = daughter_frag_n.get_num_labeled_atoms_tracer()
     corrected_intensity = intensity_m_n * (1+na*(p-m)) - intensity_m_1_n * na * ((p-d) - (m-n-1)) -\
                          intensity_m_1_n_1 * na * (d - (n-1))
+    if corrected_intensity>0:
+        return corrected_intensity
+    else:
+        return 0
+
+def arrange_fragments_by_mass(fragments_dict):
+    fragment_dict_mass = {}
+    for key, value in fragments_dict.iteritems():
+        parent_frag, daughter_frag = value[0]
+        fragment_dict_mass[(parent_frag.isotope_mass, daughter_frag.isotope_mass)] = value
+    return fragment_dict_mass
+
+def na_correction_mimosa_by_fragment(fragments_dict):
+    fragment_dict_mass = arrange_fragments_by_mass(fragments_dict)
