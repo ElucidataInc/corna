d0a1a6cc169b0bce058bc2dc663131a7e0e4659c
Sini Nagpal <sini.nagpal@elucidata.io>
2016-06-01 20:31:46 +0530

    calculated correction vector and correction matrix

--- corna/mdv_test.py
+++ corna/mdv_test.py
@@ -29,6 +29,29 @@ c2 = [0.97832, 0.02156, 0.00012]
 h4 = [0.99938, 0.00062, 0.00000015, 0.000000000015, 0.00000000000000059]
 o2 = [0.995186, 0.000738, 0.00407, 0.00000151, 0.000000416]
 c2h4 = numpy.convolve(c2, h4)
-c2h4o2 = numpy.convolve(c2h4, o2)
-print c2h4o2
+c2h4o2 = numpy.convolve(c2h4, o2)[:9]
+#correction vector
+correction_vector = c2h4o2
+
+
+#correction matrix:
+m_size = 3
+nAtom_cor = 2
+el_excluded = []
+el_cor = 'C'
+data = {'C': [0.99, 0.011], 'H' : [0.99, 0.00015], 'O': [0.99757, 0.00038, 0.00205]}
+correction_matrix = numpy.zeros((m_size, nAtom_cor+1))
+
+for i in range(nAtom_cor+1):
+    column = correction_vector[:m_size]
+    #for na in range(i):
+        #column = numpy.convolve(column, el_pur)[:m_size]
+
+    if el_excluded != el_cor:
+        for nb in range(nAtom_cor-i):
+            column = numpy.convolve(column, data[el_cor])[:m_size]
+
+    correction_matrix[:,i] = column
+
+print correction_matrix
 
