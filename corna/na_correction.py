f19dcc6a96b8cc6d7ad1c9ad507b074fd1772b06
Sini Nagpal <sini.nagpal@elucidata.io>
2016-07-07 19:47:28 +0530

    added documentation in na_correction

--- corna/na_correction.py
+++ corna/na_correction.py
@@ -1,3 +1,8 @@
+"""
+This module is a wrapper around algorithms.py. It calls different functions from algorithms.py
+and performs na correction (na_correction function). The output is given in the form of fragments
+dictionary model which can further be used in post processing function, converting to dataframr, etc
+"""
 import algorithms as algo
 import helpers as hl
 import numpy as np
@@ -11,6 +16,19 @@ def na_correction(merged_df, iso_tracers, eleme_corr, na_dict):
     This function is wrapper around algorithms.py function. It performs na correction
     for single and multiple tracers and creates the output in the form of fragment
     dictionary model with corrected intensities.
+
+    Args:
+        merged_df : dataframe with input + metadata file in long form
+
+        iso_tracer : List of isotopic tracer elements
+
+        eleme_corr : Indistinguishable species to be considered for correction
+                     along with isotopic tracers
+
+        na_dict : Dictionary of natural abundance values
+
+    Returns:
+        nacorr_dict_model : fragments dictionary with corrected intensity values
     """
     hl.convert_labels_to_std(merged_df, iso_tracers)
 
@@ -36,6 +54,10 @@ def na_correction(merged_df, iso_tracers, eleme_corr, na_dict):
 
 
 def label_corr_intens_dict(iso_tracers, trac_atoms, label_dict, formula_dict, eleme_corr, na_dict):
+    """
+    This function creates a dictionary with keys as labels and values as corrected intensities for
+    single and multiple tracer
+    """
 
     if len(trac_atoms) == 1:
         icorr = single_corr_list(label_dict, trac_atoms, formula_dict, eleme_corr, na_dict)
@@ -47,6 +69,9 @@ def label_corr_intens_dict(iso_tracers, trac_atoms, label_dict, formula_dict, el
 
 
 def corr_int_dict_model(iso_tracers, correc_inten_dict, fragments_dict):
+    """
+    This function returns the na correction dictionary model
+    """
     sample_list = algo.check_samples_ouputdict(correc_inten_dict)
     # { 0: { sample1 : val, sample2: val }, 1: {}, ...}
     lab_samp_dict = algo.label_sample_dict(sample_list, correc_inten_dict)
@@ -57,6 +82,9 @@ def corr_int_dict_model(iso_tracers, correc_inten_dict, fragments_dict):
 
 
 def single_corr_list(label_dict, trac_atoms, formula_dict, eleme_corr, na_dict):
+    """
+    This function returns the list of corrected intensities for single tracer na correction
+    """
 
     intensities = np.concatenate(np.array((label_dict).values()))
 
@@ -70,6 +98,10 @@ def single_corr_list(label_dict, trac_atoms, formula_dict, eleme_corr, na_dict):
 
 
 def singe_corr_inten_dict(icorr):
+    """
+    This function creates a dictionary of labels and corrected intensities for single tracer
+    na correction
+    """
 
     inten_index_dict = {}
     for i in range(0, len(icorr)):
@@ -79,6 +111,10 @@ def singe_corr_inten_dict(icorr):
 
 
 def multi_corr_inten_dict():
+    """
+    This function creates a dictionary of labels and corrected intensities for multi tracer
+    na correction
+    """
 
     intens_idx_dict = {}
 
@@ -86,8 +122,13 @@ def multi_corr_inten_dict():
 
 
 def multi_trac_intensities_list(no_atom_tracer, eleme_corr, eleme_corr_list, lab_dict):
-
+    """
+    This function returns the list of intensities in order of tuple list ( which contains
+    all posible combinaltion for elements to be corrected). This intensity list vector
+    is used for correction
+    """
     l = [np.arange(x+1) for x in no_atom_tracer]
+
     tup_list = list(product(*l))
 
     indist_sp = sum(eleme_corr.values(),[])
@@ -100,7 +141,10 @@ def multi_trac_intensities_list(no_atom_tracer, eleme_corr, eleme_corr_list, lab
 
 
 def multi_trac_na_correc(iso_tracers, trac_atoms, eleme_corr, formula_dict, lab_dict, na_dict):
-
+    """
+    This function returns the list of corrected intensities in order of tuple list for multi
+    tracer na correction
+    """
     if not eleme_corr:
         eleme_corr_list = trac_atoms
     else:
