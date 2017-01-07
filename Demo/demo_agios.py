186786c04847ca7a75d8007991f103b2e40aaf51
Raaisa <raaisa@elucidata.io>
2017-01-07 12:35:36 +0530

    Add comments to demo_agios.py
    This file is a demo of package and not part of the module. It
    contains commented code to show various possible usages.

--- Demo/demo_agios.py
+++ Demo/demo_agios.py
@@ -1,3 +1,4 @@
+"""This file is a demo user script for NA Correction of lcms maven input files (peak format)"""
 import os
 import corna
 
@@ -15,9 +16,6 @@ path_dir = os.path.join(os.path.dirname(__file__), 'data_agios')
 maven_data = corna.read_file(path_dir + '/testfiles/nacorr_test_1.xlsx')
 merge_mv_metdata = corna.convert_inputdata_to_stdfrom(maven_data)
 
-#print maven_data
-#test_m0_2.csv
-
 # For json input, use this funtion:
 #json_input = json.dumps(maven_data.to_dict())
 #maven_data = corna.convert_json_to_df(json_input)
@@ -27,50 +25,47 @@ merge_mv_metdata = corna.convert_inputdata_to_stdfrom(maven_data)
 
 # merge maven files and metadata files
 #merge_mv_metdata = corna.maven_merge_dfs(maven_data, maven_metadata)
-# isotopic tracers
-#iso_tracers = ['H2']
+
+#isotopic tracers
 iso_tracers = ['C13']
 
 # element to be corrected
 # in case of no indistinguishable elements, eleme corr is empty dictionary
 eleme_corr = {}
-# in case of indistinguishable elements
 
+#in case of indistinguishable elements
 #eleme_corr = {'C': ['H']}
 
 
 # NA values dict
 na_dict = corna.get_na_value_dict()
 
-# edit na values
+#edit na values
 #na_dict['H'][0] = 0.989
 
 # NA correction
 na_corr_dict = corna.na_correction(merge_mv_metdata, iso_tracers, eleme_corr, na_dict)
 na_corr_df = corna.convert_to_df(na_corr_dict, parent=False, colname='NA corrected')
-#working till this point
 
 
 # Replace negative values by zero on NA corrected data - optional
-print 'before post processed'
 postprocessed_out = corna.replace_negatives(na_corr_dict)
 postprocessed_out_df = corna.convert_to_df(postprocessed_out, parent=False, colname='CorrIntensities-Replaced_negatives')
-print 'postprocessed_out_df'
 
 # calculate fractional enrichment on post processed data
-print 'before frac'
 frac_enrichment = corna.fractional_enrichment(postprocessed_out)
-print 'frac'
 frac_enr_df = corna.convert_to_df(frac_enrichment, parent=False, colname='Frac Enrichment')
-print 'frac comp'
+
 # # combine results - dataframe with na correction column, frac enrichment column and post processed column
 df_list = [na_corr_df, frac_enr_df, postprocessed_out_df, merge_mv_metdata]
 merged_results_df = corna.merge_multiple_dfs(df_list)
 print merged_results_df
-# # filter any dataframe as per requirement.
-# # any number of columns and column values can be filtered
+
+# filter any dataframe as per requirement.
+# any number of columns and column values can be filtered
 # col_rename = {'Name': ['L-Methionine'], "Sample": ['sample_1', 'sample_2']}
 # filtered_data = corna.filter_df(merged_results_df, col_rename)
-# # save any dataframe at given path
+
+# save any dataframe at given path
 # save_dfs = corna.save_to_csv(merged_results_df, path_dir + 'results.csv')
-#
+
