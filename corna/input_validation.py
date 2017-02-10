7f389bf92a723017cb181abc0c2bcd07e1bdba9c
Ankur Agrawal <ankur.agrawal@elucidata.io>
2017-02-10 18:56:54 +0530

    Add description to the function check_required_column

--- corna/input_validation.py
+++ corna/input_validation.py
@@ -1,4 +1,14 @@
 def check_required_column(data_frame,*arg):
+    """
+    This function takes data frame and column_name as an argumnet.
+    It checks whether the required columns is present in a data_
+    farme or not. It will populates the list of all the column name
+    which are not present in data frame.
+
+    :param data_frame: The data frame which we need to assert
+    :param arg: all the column name as argument
+    :return: missing column list
+    """
     required_column_names=[column_name.upper() for column_name in arg]
     data_frame_columns=[column_name.upper() for column_name in data_frame.columns.tolist()]
     missing_column=list()
