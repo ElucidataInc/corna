f786eb6ba18fc6fb1262c357fc2d7e8589c56319
Ankur Agrawal <ankur.agrawal@elucidata.io>
2017-02-13 18:28:33 +0530

    test : test case for function check_file_empty is added

--- tests/test_input_validation.py
+++ tests/test_input_validation.py
@@ -1,18 +1,26 @@
 from corna.custom_exception import FileExtensionError,FileEmptyError
 from corna.custom_exception import MissingRequiredColumnError,FileExistError
 from corna.custom_exception import DataFrameEmptyError
-from corna.input_validation import check_if_file_exist
+from corna.input_validation import check_if_file_exist,check_file_empty
 import pytest
 import os
 
 
-class TestClass():
+def test_check_if_file_exist():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    file_not_exist_path = os.path.join(dir_path, "test_input_validation_data", "maven_data.py")
 
-    def test_check_if_file_exist(self):
-        dir_path = os.path.dirname(os.path.abspath(__file__))
-        file_not_exist_path = os.path.join(dir_path,"test_input_validation_data" ,"maven_data.py")
+    with pytest.raises(FileExistError) as e:
+        check_if_file_exist(file_not_exist_path)
 
-        with pytest.raises(FileExistError) as e:
-            check_if_file_exist(file_not_exist_path)
+    assert e.value.message == 'The file does not exist. Check again.'
 
-        assert e.value.message == 'The file does not exist. Check again.'
+
+def test_check_file_empty():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    empty_file_path = os.path.join(dir_path, "test_input_validation_data", "nacorr_test_1.txt")
+
+    with pytest.raises(FileEmptyError) as e:
+        check_file_empty(empty_file_path)
+
+    assert e.value.message=='The file is empty. No data to process.'
