d7c2e663d3b6f7a681f018bfe86e1b20499a035f
Ankur Agrawal <ankur.agrawal@elucidata.io>
2017-02-14 15:03:03 +0530

    ftr : schema for apllying validation checks is added
    - validator_column_wise will take column list and callable function list
    on which check is to be applied.
    
    -check_missing will return the resulatnt data_frame containg information of
    the cell which has blank value.

--- corna/input_validation.py
+++ corna/input_validation.py
@@ -30,3 +30,45 @@ def validate_input_file(path,required_columns_name):
         raise MissingRequiredColumnError(missing_columns)
 
     return data_frame
+
+@handleError
+def validator_column_wise(input_data_frame, axis=0, column_list=[], function_list=[]):
+    """
+    This is basically a schema for performing column wise validation checks.
+    Validaton functions are passed as an argumnet.
+    The resultant data frame is returned which contains state for cell
+    which function is applied.
+
+    :param input_data_frame: the data frame on which validation is to be applied
+    :param axis: for defining row wise or column wise operations
+    :param column_list: column of data frame on which validation is to be applied
+    :param function_list: list of validatin function
+    :return: resultant dataframe
+    """
+    resultant_dataframe=pd.DataFrame()
+    for function in function_list:
+        column_dataframe=pd.DataFrame()
+        for column in column_list:
+            function_name=function.__name__
+            column_dataframe['status']=input_data_frame[column].apply(function)
+            column_dataframe['column_name'],column_dataframe['function_name']=column,function_name
+            resultant_dataframe=resultant_dataframe.append(column_dataframe)
+    return resultant_dataframe
+
+@handleError
+def check_missing(input_data_frame):
+    """
+    This function returns the data frame containing state of every cell which has
+    missing value. We are using pandas.isnull method.
+    
+    :param input_data_frame:
+    :return: resultant data frame
+    """
+    missing_dataframe = input_data_frame.isnull()
+    missing_dataframe['function_name'] = 'missing'
+    missing_dataframe['row_number'] = missing_dataframe.index
+    resultant_dataframe= pd.melt(missing_dataframe, id_vars=['function_name', 'row_number'],
+                                 var_name='column_name')
+    return resultant_dataframe
+
+
