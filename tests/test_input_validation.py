568db0134b21851aa6aaa32d5b9bfa337105a64d
Ankur Agrawal <ankur.agrawal@elucidata.io>
2017-02-20 20:27:27 +0530

    test: add test cases for input_validation function
    -all the files required for test are uploaded in directory
     test_input_validation_data

--- tests/test_input_validation.py
+++ tests/test_input_validation.py
@@ -3,10 +3,16 @@ from corna.custom_exception import MissingRequiredColumnError,FileExistError
 from corna.custom_exception import DataFrameEmptyError
 from corna.dataframe_validator import check_if_file_exist,check_file_empty
 from corna.dataframe_validator import read_input_file,check_data_frame_empty
-from corna.input_validation import validate_input_file
+from corna.input_validation import validate_input_file,check_missing
+from corna.input_validation import check_duplicate,get_label,check_label_in_formula
+from corna.input_validation import check_postive_numerical_value,check_label_column_format
+from corna.input_validation import check_formula_is_correct,validator_for_two_column
+from corna.input_validation import validator_column_wise
+import corna.constants as constant
 import pytest
 import os
 import pandas as pd
+
 
 def test_check_if_file_exist():
     dir_path = os.path.dirname(os.path.abspath(__file__))
@@ -25,7 +31,7 @@ def test_check_file_empty():
     with pytest.raises(FileEmptyError) as e:
         check_file_empty(empty_file_path)
 
-    assert e.value.message=='The file is empty. No data to process.'
+    assert e.value.message == 'The file is empty. No data to process.'
 
 def test_read_input_file():
     dir_path = os.path.dirname(os.path.abspath(__file__))
@@ -34,7 +40,7 @@ def test_read_input_file():
     with pytest.raises(FileExtensionError) as e:
         read_input_file(other_extension_file_path)
 
-    assert e.value.message=='Only CSV , TXT , XLS ,XLSX file extension are allowed.'
+    assert e.value.message == 'Only CSV , TXT , XLS ,XLSX file extension are allowed.'
 
 def test_data_frame_empty():
     data_frame=pd.DataFrame()
@@ -42,14 +48,93 @@ def test_data_frame_empty():
     with pytest.raises(DataFrameEmptyError) as e:
         check_data_frame_empty(data_frame)
 
-    assert e.value.message=='There is no data to process.'
+    assert e.value.message == 'There is no data to process.'
 
 def test_required_column_missing():
     dir_path = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(dir_path, "test_input_validation_data", "nacorr_test_1.xlsx")
-    test_required_column=('NaMe','LABEl','Formula','XYZ')
+    test_required_column = ('NaMe','LABEl','Formula','XYZ')
 
     with pytest.raises(MissingRequiredColumnError) as e:
         validate_input_file(file_path,test_required_column)
 
-    assert e.value.message=='The required column XYZ are not present'
+    assert e.value.message == 'The required column XYZ are not present'
+
+
+def test_check_positive_numerical_value():
+
+    assert check_postive_numerical_value(14) == 'correct'
+    assert check_postive_numerical_value(-19) == 'negative'
+    assert check_postive_numerical_value('test123') == 'invalid_intensity_value'
+
+
+def test_check_label_column_format():
+
+    assert check_label_column_format('C12 PARENT') == 'correct'
+    assert check_label_column_format('C13-label-2') == 'correct'
+    assert check_label_column_format('C13-2') == 'invalid_label'
+    assert check_label_column_format('C13-label-a') == 'invalid_label'
+    assert check_label_column_format('C11-N15-label-1-2') == 'invalid_label'
+
+
+def test_label_in_formula():
+
+    assert check_label_in_formula('C13-label-2','C10H12N5O6P') == 'correct'
+    assert check_label_in_formula('C13-label-6', 'C5H12N5O6P') == 'element_in_label_more_than_formula'
+    assert check_label_in_formula('C13-label-6', 'H12N5O6P') == 'label_not_in_formula'
+
+
+def test_get_label():
+
+    assert get_label('C12 PARENT') == {'C': 1}
+    assert get_label('C13-label-6') == {'C': 6}
+    assert get_label('C13N15-label-4-5') == {'C': 4, 'N': 5}
+
+
+def test_check_formula_is_correct():
+
+    assert check_formula_is_correct('C5H12N5O6P') == 'correct'
+    assert check_formula_is_correct('C5H12N5O6PLO') == 'invalid_formula'
+
+
+def test_check_duplicate():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    maven_correct_raw_file = os.path.join(dir_path, "test_input_validation_data",
+                                          "test_maven_upload_acetic.csv")
+    maven_df_correct = pd.read_csv(maven_correct_raw_file)
+    maven_duplicate_entry_file=os.path.join(dir_path, "test_input_validation_data",
+                                            "test_maven_upload_duplicate_entry.csv")
+    maven_df_duplicate_entry=pd.read_csv(maven_duplicate_entry_file)
+
+    assert len(check_duplicate(maven_df_duplicate_entry,0,[['Name','Label']])) == 2
+    assert check_duplicate(maven_df_correct,0,[['Name','Label']]).empty
+
+
+def test_check_missing():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    maven_missing_entry_raw_file = os.path.join(dir_path, "test_input_validation_data",
+                                                "test_maven_upload_missing_entry.csv")
+    maven_missing_entry_df = pd.read_csv(maven_missing_entry_raw_file)
+    assert len(check_missing(maven_missing_entry_df)) == 2
+
+def test_validator_for_two_column():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    maven_raw_label_not_in_formula_raw_file = os.path.join(dir_path, "test_input_validation_data",
+                                                "test_maven_upload_acetic_label_incorrect.csv")
+    maven_df = pd.read_csv(maven_raw_label_not_in_formula_raw_file)
+
+    result_df = validator_for_two_column(maven_df,constant.LABEL_COL,constant.FORMULA_COL,
+                                check_label_in_formula)
+
+    assert result_df.iloc[0]['row_number'] == 1
+
+def test_validator_column_wise():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    maven_raw_intensity_not_correct_raw_file = os.path.join(dir_path, "test_input_validation_data",
+                                                           "test_maven_upload_acetic_intensity_incorrect.csv")
+    maven_df = pd.read_csv(maven_raw_intensity_not_correct_raw_file)
+
+    result_df = validator_column_wise(maven_df,0,['sample_1'],[check_postive_numerical_value])
+    assert result_df.iloc[0]['state'] == 'negative'
+    assert result_df.iloc[1]['state'] == 'invalid_intensity_value'
+
