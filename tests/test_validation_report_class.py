10c77c50d5cd7b412b776a5326a09a1ce1680aa1
Ankur Agrawal <ankur.agrawal@elucidata.io>
2017-02-28 13:53:45 +0530

    Test: Add test function for validation_report_class methods

--- tests/test_validation_report_class.py
+++ tests/test_validation_report_class.py
@@ -1,8 +1,10 @@
-from corna.validation_report_class import ValidationReport
+import os
 import pandas as pd
 
+from corna.validation_report_class import ValidationReport
 
-class TestValidationReport:
+
+class TestValidationReport():
 
     @classmethod
     def setup_class(cls):
@@ -33,12 +35,42 @@ class TestValidationReport:
         df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
         self.validation.report_dataframe = df
         assert self.validation.decide_action()['action'] == 'Row_Wise_Action'
+        print self.validation.action
 
     def test_take_action(self):
+
         df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
-        self.validation.report_dataframe = df
+        self.action = {'action': 'Row_Wise_Action', 0: [{'column': 'Name', 'action': 'DROP', 'state': 'missing'}]}
+
         new_df = self.validation.take_action(df)
         assert new_df.empty
+
+    def test_action_drop_rows(self):
+
+        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        assert self.validation.action_drop_rows(df,[0]).empty
+
+    def test_get_unique_row_having_error(self):
+        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        self.validation.report_dataframe = df
+
+        assert self.validation.get_unique_row_having_error(self.validation) == [0]
+
+    def test_get_slice_df_with_row(self):
+        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+
+        assert self.validation.get_slice_df_with_row(df, 0).empty == False
+
+    def test_get_key_list(self):
+        dict = {'A':1,'B':2}
+
+        assert self.validation.get_key_list(dict) == ['A','B']
+
+    def test_get_action_name(self):
+        result = ['Sample','Invalid']
+
+        assert self.validation.get_action_name(result) == 'Stop_Tool'
+
 
 
 
