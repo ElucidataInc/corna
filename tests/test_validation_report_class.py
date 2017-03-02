126b610fff6061dd6a1bbab82b8914b622ca8c69
Ankur Agrawal <ankur.agrawal@elucidata.io>
2017-03-02 17:20:26 +0530

    Test: Add fixture for generating test_df

--- tests/test_validation_report_class.py
+++ tests/test_validation_report_class.py
@@ -1,7 +1,13 @@
 import os
 import pandas as pd
+import pytest
 
 from corna.validation_report_class import ValidationReport
+
+@pytest.fixture()
+def get_test_df():
+    df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+    return df
 
 
 class TestValidationReport():
@@ -10,61 +16,61 @@ class TestValidationReport():
     def setup_class(cls):
         cls.validation=ValidationReport()
 
+
     @classmethod
     def teardown_class(cls):
         del cls.validation
 
     def test_append(self):
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
         self.validation.append(df)
         assert self.validation.report_df.equals(df)
 
     def test_generate_report(self):
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
         self.validation.report_dataframe = df
         test_result = {0: {'warning': [['Name', 'missing']], 'errors': []}}
         assert self.validation.generate_report() == test_result
 
+
     def test_generate_action(self):
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
         self.validation.report_dataframe = df
         test_result = {0: {'warning': [['Name', 'missing', 'DROP']], 'errors': []}}
         assert self.validation.generate_action() == test_result
 
     def test_decide_action(self):
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
         self.validation.report_dataframe = df
         assert self.validation.decide_action()['action'] == 'Row_Wise_Action'
         print self.validation.action
 
     def test_take_action(self):
-
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
         self.action = {'action': 'Row_Wise_Action', 0: [{'column': 'Name', 'action': 'DROP', 'state': 'missing'}]}
 
         new_df = self.validation.take_action(df)
         assert new_df.empty
 
     def test_action_drop_rows(self):
-
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
         assert self.validation.action_drop_rows(df,[0]).empty
 
     def test_get_unique_row_having_error(self):
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
         self.validation.report_dataframe = df
 
         assert self.validation.get_unique_row_having_error(self.validation) == [0]
 
     def test_get_slice_df_with_row(self):
-        df = pd.DataFrame({'column_name': 'Name', 'row_number': 0, 'state': 'missing'}, index=[0])
+        df = get_test_df()
 
         assert self.validation.get_slice_df_with_row(df, 0).empty == False
 
     def test_get_key_list(self):
-        dict = {'A':1,'B':2}
+        input_dict = {'A':1,'B':2}
 
-        assert self.validation.get_key_list(dict) == ['A','B']
+        assert self.validation.get_key_list(input_dict) == ['A','B']
 
     def test_get_action_name(self):
         result = ['Sample','Invalid']
