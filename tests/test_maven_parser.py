f1014c50f7daac0670c115fdaf690047b3429cd6
Ankur Agrawal <ankur.agrawal@elucidata.io>
2017-03-04 12:29:11 +0530

    Test: Change test functions
    - Fixtures are implemented in correct way

--- tests/test_maven_parser.py
+++ tests/test_maven_parser.py
@@ -6,20 +6,20 @@ from pandas.util.testing import assert_frame_equal
 
 from corna import custom_exception
 from corna.inputs import maven_parser
-from tests import constants
-from tests import fixtures
+import constants
+from fixtures import *
 
 
-def test_read_input_file_all_correct():
+def test_read_input_file_all_correct(get_mergedf_all_correct):
     result_df, result_log, _ = maven_parser.read_maven_file(constants.MAVEN_FILE, constants.METADATA_FILE)
-    test_df = fixtures.get_mergedf_all_correct()
+    test_df = get_mergedf_all_correct
     test_log = {'warning': {'action': [], 'message': []}, 'errors': []}
     assert result_log == test_log
     assert result_df.equals(test_df)
 
 
-def test_read_input_file_no_metadata():
-    test_df = fixtures.get_mergedf_no_metadata()
+def test_read_input_file_no_metadata(get_mergedf_no_metadata):
+    test_df = get_mergedf_no_metadata
     test_log = {'warning': {'action': [], 'message': []}, 'errors': []}
     result_df, result_log, _ = maven_parser.read_maven_file(constants.MAVEN_FILE, None)
     assert result_log == test_log
@@ -38,21 +38,21 @@ def test_read_input_file_error_in_maven_file():
     assert result_df.empty
 
 
-def test_read_input_file_warning_in_maven():
+def test_read_input_file_warning_in_maven(get_mergedf_warning):
     maven_file_path = constants.MAVEN_FILE_PATH_DUPLICATE_ENTRY
     result_df, result_log, _ = maven_parser.read_maven_file(maven_file_path, constants.METADATA_FILE)
     test_log = {'warning': {'action': ['Row is Dropped', 'Row is Dropped'], 'message':
         ['Row Number <b>3</b> : column <b>Name-Label</b> has <b>duplicate</b> value',
          'Row Number <b>4</b> : column <b>Name-Label</b> has <b>duplicate</b> value']}, 'errors': []}
-    test_df = fixtures.get_mergedf_warning()
+    test_df = get_mergedf_warning
     assert result_log == test_log
     assert result_df.equals(test_df)
 
 
-def test_filtered_data_frame():
-    maven_df = fixtures.get_maven_file_extra_sample()
-    metadata_df = fixtures.get_metadata_df()
-    test_df = fixtures.get_maven_df()
+def test_filtered_data_frame(get_maven_file_extra_sample, get_metadata_df, get_maven_df):
+    maven_df = get_maven_file_extra_sample
+    metadata_df = get_metadata_df
+    test_df = get_maven_df
     result_df = maven_parser.filtered_data_frame(maven_df, metadata_df)
 
     assert_frame_equal(result_df.sort(axis=1), test_df.sort(axis=1), check_names=True)
@@ -95,9 +95,9 @@ def test_check_error_present():
     assert maven_parser.check_error_present(logs)
 
 
-def test_filtered_data_frame_empty_intersection():
-    maven_df = fixtures.get_maven_file_empty_intersection()
-    metadata_df = fixtures.get_metadata_df()
+def test_filtered_data_frame_empty_intersection(get_maven_file_empty_intersection, get_metadata_df):
+    maven_df = get_maven_file_empty_intersection
+    metadata_df = get_metadata_df
 
     with pytest.raises(custom_exception.NoIntersectionError) as e:
         maven_parser.filtered_data_frame(maven_df, metadata_df)
@@ -109,33 +109,33 @@ def test_basic_validation():
     assert maven_parser.check_basic_validation(maven_file_path)
 
 
-def test_column_name_set():
-    maven_df = fixtures.get_maven_df()
+def test_column_name_set(get_maven_df):
+    maven_df = get_maven_df
     assert maven_parser.get_column_names_set(maven_df) == set(['Formula',
                                                                'Name', 'sample_1', 'Label'])
 
 
-def test_unique_column_value():
-    maven_df = fixtures.get_maven_df()
+def test_unique_column_value(get_maven_df):
+    maven_df = get_maven_df
     assert maven_parser.get_unique_column_value(maven_df, 'Name') == set(['Acetic'])
 
 
-def test_drop_duplicates():
-    maven_df = fixtures.get_maven_df()
+def test_drop_duplicates(get_maven_df):
+    maven_df = get_maven_df
     test_df = pd.DataFrame({'Name': ['Acetic'], 'Formula': ['H4C2O2N'],
                             'Label': ['C12 PARENT'], 'sample_1': [0.2274]})
     result_df = maven_parser.drop_duplicate_rows(maven_df, 'Name')
     assert_frame_equal(result_df.sort(axis=1), test_df.sort(axis=1), check_names=True)
 
 
-def test_get_metadata_df():
-    test_df = fixtures.get_metadata_df()
+def test_get_metadata_df(get_metadata_df):
+    test_df = get_metadata_df
     result_df = maven_parser.get_metadata_df(constants.METADATA_FILE)
     assert_frame_equal(result_df.sort(axis=1), test_df.sort(axis=1), check_names=True)
 
 
-def test_get_sample_column():
-    maven_df = fixtures.get_maven_df()
+def test_get_sample_column(get_maven_df):
+    maven_df = get_maven_df
     assert maven_parser.get_sample_column(maven_df) == ['sample_1']
 
 
@@ -148,12 +148,12 @@ def test_get_extracted_isotracer():
     assert maven_parser.get_extracted_isotracer('C12 PARENT') == 'C13N15'
 
 
-def test_get_extraced_isotracer_df():
-    maven_df = fixtures.get_maven_df()
+def test_get_extraced_isotracer_df(get_maven_df):
+    maven_df = get_maven_df
     test_assert = ['C13N15', 'C13', 'C13']
     assert list(maven_parser.get_extraced_isotracer_df(maven_df)) == test_assert
 
 
-def test_isotracer_dict():
-    maven_df = fixtures.get_maven_df()
+def test_isotracer_dict(get_maven_df):
+    maven_df = get_maven_df
     assert maven_parser.get_isotracer_dict(maven_df) == {'C13': 2, 'C13N15': 1}
