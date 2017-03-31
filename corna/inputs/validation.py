bd27c684a79ca343fb90c8aff62ade7a9bb0f92c
yugi003 <yugantar.kumar@elucidata.io>
2017-03-31 10:59:10 +0530

    add function to validate file using olmonk package
    created a new module validation.py. It has a task to take file
    to do basic_validation and then data_validation and sends back
    the logs with validated df.

---
+++ corna/inputs/validation.py
@@ -0,0 +1,41 @@
+"""This module helps to do validation using validation package olmonk"""
+
+from corna.olmonk import basic_validation
+
+def get_validation_df(path):
+	"""takes path of the file, validates it and returns result
+
+	Using path of the file, it instantiates the validation class
+	and use its methods to validate file and returns result of
+	that. If file not passes the validation checks, then it will
+	raise an error, otherwise it will returns validated df.
+	Args:
+		path: file path for which validation is needed
+
+	Returns: validated df
+	"""
+	try:
+		basic_validator = get_class_inst(basic_validation.BasicValidator, path)
+		basic_validation_result(basic_validator)
+		return basic_validator.df
+	except Exception as e:
+		raise
+
+def get_class_inst(validator_class, class_arg):
+	"""Instantiates class with its argument and returns its instance"""
+
+	return validator_class(class_arg)
+
+def basic_validation_result(basic_validator):
+	"""Takes instance of class, do basic validation and raise error
+	if any check fails
+	"""
+
+	try:
+		basic_validator.check_path_exist()
+		basic_validator.check_file_empty()
+		basic_validator.check_if_convert_to_df()
+	except Exception as e:
+		raise
+
+
