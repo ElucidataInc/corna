13f06106bf729bbdb9af7c8df97dd93232913341
Raaisa <raaisa@elucidata.io>
2017-06-02 16:24:25 +0530

    fix: Remove comments

--- corna/__init__.py
+++ corna/__init__.py
@@ -8,13 +8,3 @@ from .inputs.maven_parser import read_maven_file
 from .inputs.multiquant_parser import merge_mq_metadata, mq_df_to_fragmentdict, get_validated_df_and_logs
 from .output import convert_to_df, save_to_csv
 from .postprocess import replace_negatives, fractional_enrichment
-
-# from . corna_agios.na_correction import na_correction
-
-#from . corna_yale.file_parser_yale import read_multiquant, read_multiquant_metadata, merge_mq_metadata
-
-#from . corna_yale.preprocess import met_background_correction, met_background_correction_all
-
-#from . corna_yale.algorithms_yale import na_correction_mimosa
-
-#:TODO chnage import, this not the good way
