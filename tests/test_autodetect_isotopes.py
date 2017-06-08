4730441c4e59035740788a2bf8bf2c26192996b4
Nikita1093 <nikita.gupta@elucidata.io>
2017-06-08 15:04:49 +0530

    [ref]: Change name of the tests

--- tests/test_autodetect_isotopes.py
+++ tests/test_autodetect_isotopes.py
@@ -11,41 +11,41 @@ def test_get_elements_from_formula():
     assert auto.get_elements_from_formula('C6H6') == ['H', 'C']
 
 
-def test_get_indistinguishable_ele():
-    assert auto.get_indistinguishable_ele('C', 'N', 'C6H6N', 600) is None
+def test_fetch_indistinguishable_ele():
+    assert auto.get_indistinguishable_ele('C', 'N', 'C6H6N', 600) == None
 
 
 def test_get_indistinguishable_ele():
-    assert auto.get_indistinguishable_ele('C', 'N', 'C16H16O23N12',  2000) is 'N'
+    assert auto.get_indistinguishable_ele('C', 'N', 'C16H16O23N12',  2000) == 'N'
 
 
 def test_ppm_validation():
-    assert auto.ppm_validation(10.5, 10, 'C9H9', 'H') is True
+    assert auto.ppm_validation(10.5, 10, 'C9H9', 'H') == True
 
 
-def test_ppm_validation():
-    assert auto.ppm_validation(100, 10, 'C9H9', 'H') is True
+def test_ppm_validation_True():
+    assert auto.ppm_validation(100, 10, 'C9H9', 'H') == True
 
 
-def test_ppm_validation():
-    assert auto.ppm_validation(1, 10, 'C9H9', 'H') is None
+def test_ppm_validation_False():
+    assert auto.ppm_validation(1, 10, 'C9H9', 'H') == None
 
 
 def get_element_correction_dict():
     assert get_element_correction_dict(400, 'C14H65O9', ['C13']) == {'C': ['H', 'O']}
 
-def test_borderline_ppm_warning():
-    assert auto.borderline_ppm_warning(30, 30.1, 'C5H5', 'H') is True
+def test_raise_borderline_ppm_warning():
+    assert auto.borderline_ppm_warning(30, 30.1, 'C5H5', 'H') == True
 
 
 def test_borderline_ppm_warning():
-    assert auto.borderline_ppm_warning(30, 300.1, 'C5H5', 'H') is None
+    assert auto.borderline_ppm_warning(30, 300.1, 'C5H5', 'H') == None
 
 def test_get_mass_diff():
-    assert auto.get_mass_diff('C','N') is 0.00631
+    assert auto.get_mass_diff('C','N') == 0.00631
 
-def test_get_mass_diff():
-    assert auto.get_mass_diff('C','P') is None
+def test_get_mass_diff_Keyerror():
+    assert auto.get_mass_diff('C','P') == None
 
 
 
