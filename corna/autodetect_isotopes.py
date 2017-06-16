7c7d5e0356885c66575dcb2d8211229e8ac84cff
Nikita1093 <nikita.gupta@elucidata.io>
2017-06-16 12:52:27 +0530

    [Add]: add_isotopes_list()
    This function creates the list of isotopes of elements present in metabolite's formula.

--- corna/autodetect_isotopes.py
+++ corna/autodetect_isotopes.py
@@ -1,5 +1,3 @@
-import warnings
-
 import constants as cs
 import helpers as hl
 from model import Ion
@@ -41,7 +39,7 @@ def borderline_ppm_warning(ppm_user_input, required_ppm, formula, ele):
         UserWarning: if borderline conditions are met
     """
     if (required_ppm - cs.BORDERLINE_LIMIT) <= ppm_user_input <= (required_ppm + cs.BORDERLINE_LIMIT):
-        warnings.warn(cs.PPM_REQUIREMENT_VALIDATION + formula + ':' + ele, UserWarning)
+        print cs.PPM_REQUIREMENT_VALIDATION + formula + ':' + ele
         return True
 
 
@@ -105,6 +103,7 @@ def get_indistinguishable_ele(isotracer, formula, ppm_user_input,element):
         if ppm_validation(ppm_user_input, required_ppm, formula, element):
             return element
 
+
 def get_isotope_element_list(isotracer):
     """
     This function returns list of elements present in labelled
@@ -124,6 +123,26 @@ def get_isotope_element_list(isotracer):
     return isotracer_ele_list
 
 
+def add_isotopes_list(indis_ele_list):
+    """
+    This function adds isotopes of the element
+    present in the formula. These isotopes are
+    then tested for indistinguishable isotopes.
+    Args:
+        indis_ele_list: List of indistinguishable
+        elements. 
+
+    Returns:
+        list with isotopes of the elements.
+
+    """
+    for ele in indis_ele_list:
+        if ele in cs.ISOTOPE_DICT.keys():
+            indis_ele_list_isotopes = list((set(indis_ele_list)-set([ele])) | set(cs.ISOTOPE_DICT[ele]))
+    return indis_ele_list_isotopes
+
+
+
 def get_element_correction_dict(ppm_user_input, formula, isotracer):
     """This function returns a dictionary with all isotracer elements
     as key and indistinguishable isotopes as values.
@@ -141,13 +160,15 @@ def get_element_correction_dict(ppm_user_input, formula, isotracer):
     ion_object = Ion('', formula)
     ele_list = (ion_object.get_formula()).keys()
     isotracer_list = get_isotope_element_list(isotracer)
+    isotope_ele = get_isotope_element_list(cs.MASS_DIFF_DICT.keys())
     ele_list_without_isotracer = set(ele_list) - set(isotracer_list)
-    isotracer_list = list(set(isotracer_list).intersection(set(ele_list)))
-    for isotope in isotracer_list:
-            element_correction_dict[isotope] = []
-            indis_ele_list = list(ele_list_without_isotracer.intersection(set(cs.MASS_DIFF_DICT.keys())))
+    for isotope in isotracer:
+        if isotope[0] in ele_list:
+            indis_ele_list = list(ele_list_without_isotracer.intersection(set(isotope_ele)))
+            indis_ele_list = add_isotopes_list(indis_ele_list)
             get_ele = lambda iso: get_indistinguishable_ele(isotope, formula, ppm_user_input, iso)
             indis_element = map(get_ele, indis_ele_list)
             indis_element = filter(None, indis_element)
-            element_correction_dict[isotope] = indis_element
+            element_correction_dict[isotope[0]] = indis_element
+
     return element_correction_dict
