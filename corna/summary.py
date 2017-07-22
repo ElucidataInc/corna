7e2ee640c1eba79c5161eeaff86fd00b8433b496
Nikita1093 <nikita.gupta@elucidata.io>
2017-07-22 17:55:01 +0530

    [Add]: LCMS summary functions

--- corna/summary.py
+++ corna/summary.py
@@ -43,3 +43,33 @@ def summary_sample_metadata(metadata_std_df):
         dict2['value'] = dict1[dict_label_list[i]]
         summary_list.append(dict2)
     return summary_list
+
+
+def summary_raw_intensity_lcms(raw_intensity_df):
+    summary_list = []
+    dict1 = {'Number of metabolites': len(raw_intensity_df['Name'].unique()),
+             'Number of samples': (len(list(raw_intensity_df)) - 3),
+             'Number of blank intensity cells': raw_intensity_df.isnull().values.ravel().sum(),
+             #'Number of labelled elements': len(raw_intensity_df['Component Name'].unique()),
+             'Number of rows': raw_intensity_df['Label'].count()}
+    dict_label_list = dict1.keys()
+    for i in range(0, len(dict_label_list)):
+        dict2 = {}
+        dict2['label'] = dict_label_list[i]
+        dict2['value'] = dict1[dict_label_list[i]]
+        summary_list.append(dict2)
+    return summary_list
+
+def lcms_metadata(meta_df):
+    summary_list = []
+    dict1 = {'Fields in metadata': ", ".join(list(meta_df)),
+             'Number of rows in metadata': list(meta_df[[0]].count())[0],
+             }
+    dict_label_list = dict1.keys()
+    for i in range(0, len(dict_label_list)):
+        dict2 = {}
+        dict2['label'] = dict_label_list[i]
+        dict2['value'] = dict1[dict_label_list[i]]
+        summary_list.append(dict2)
+    return summary_list
+
