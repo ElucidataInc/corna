6c10b4f214dc402d362eef1273c5da158d13837e
Nikita1093 <nikita.gupta@elucidata.io>
2017-07-22 16:56:37 +0530

    [Add]: functions for lcms/ms summary

---
+++ corna/summary.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def summary_raw_intensity_msms(raw_intensity_df):
+    summary_list = []
+    dict1 = {'Number of rows': raw_intensity_df['Original Filename'].count(),
+             'Number of samples': len(raw_intensity_df['Original Filename'].unique()),
+             'Number of cohorts': len(raw_intensity_df['Sample Name'].unique()),
+             'Number of metabolites': len(raw_intensity_df['Component Name'].unique())}
+    dict_label_list = dict1.keys()
+    for i in range(0, len(dict_label_list)):
+        dict2 = {}
+        dict2['label'] = dict_label_list[i]
+        dict2['value'] = dict1[dict_label_list[i]]
+        summary_list.append(dict2)
+    return summary_list
+
+
+def summary_metadata_mq(metadata_mq_df):
+    summary_list = []
+    dict1 = {'Number of fragments': metadata_mq_df['Component Name'].count(),
+             'Number of unlabeled fragments': len(metadata_mq_df['Unlabeled Fragment'].unique()),
+             'isotopic tracer': list((metadata_mq_df['Isotopic Tracer'].unique()))
+            }
+    dict_label_list = dict1.keys()
+    for i in range(0, len(dict_label_list)):
+        dict2 = {}
+        dict2['label'] = dict_label_list[i]
+        dict2['value'] = dict1[dict_label_list[i]]
+        summary_list.append(dict2)
+    return summary_list
+
+
+def summary_sample_metadata(metadata_std_df):
+    summary_list = []
+    dict1 = {'Number of background samples': len(metadata_std_df['Background Sample'].unique()),
+             'Fields in metadata': ", ".join(list(metadata_std_df)),
+            }
+    dict_label_list = dict1.keys()
+    for i in range(0, len(dict_label_list)):
+        dict2 = {}
+        dict2['label'] = dict_label_list[i]
+        dict2['value'] = dict1[dict_label_list[i]]
+        summary_list.append(dict2)
+    return summary_list
