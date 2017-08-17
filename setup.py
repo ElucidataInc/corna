c00761e2fefdd5417e1b60b0e3ab918c2fd5f9fb
Ankur Agrawal <ankur@Ankurs-MacBook-Pro.local>
2017-08-17 19:38:43 +0530

    [ver]: Change version from 0.1.10 to 0.1.11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
 	name='corna',
-    version='0.1.10',
+    version='0.1.11',
     description='Natural Abundance Correction Toolbox',
     long_description=readme(),
     packages = find_packages(),
