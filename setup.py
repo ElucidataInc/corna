6f489e8bec6b6abee981f4785c00aa76ef5ac038
Ankur Agrawal <ankur@Ankurs-MacBook-Pro.local>
2017-12-27 18:15:27 +0530

    [ver]: Change version in setup from 0.1.32 to 0.1.33

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
 	name='corna',
-    version='0.1.32',
+    version='0.1.34',
     description='Natural Abundance Correction Toolbox',
     long_description=readme(),
     packages = find_packages(),
