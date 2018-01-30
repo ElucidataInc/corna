874d1a5ec0f394d728581822601b9c30be740f99
tyagi_harshit <harshit.tyagi@elucidata.io>
2018-01-30 11:54:09 +0530

    [ver]:  update the status

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
 	name='corna',
-    version='0.0.105',
+    version='0.1.48',
     description='Natural Abundance Correction Toolbox',
     long_description=readme(),
     packages = find_packages(),
